f9fdb9ca97f58f0b59004ad7580506e0c583d46b
Christopher Meier <christopher.meier@heig-vd.ch>
2017-12-09 12:51:09 +0100

    Added endpoint for tests. Corrected configuration.

--- swagger/spring-server-test/src/main/java/io/lozzikit/mail/api/TestApiController.java
+++ swagger/spring-server-test/src/main/java/io/lozzikit/mail/api/TestApiController.java
@@ -2,10 +2,57 @@ package io.lozzikit.mail.api;
 
 import io.lozzikit.mail.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestApiController {
     @Autowired
     private TestService testService;
+
+    @RequestMapping(value = "/tests/templates",
+            method = RequestMethod.DELETE)
+    public ResponseEntity<Object> dbTemplatesDelete() {
+        testService.deleteTemplates();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/tests/templates",
+            method = RequestMethod.POST)
+    public ResponseEntity<Object> dbTemplatesPopulate(@RequestBody String set) {
+        if(testService.populateTemplates(set)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/tests/mails+jobs",
+            method = RequestMethod.DELETE)
+    public ResponseEntity<Object> dbMailsAndJobsDelete() {
+        testService.deleteJobs();
+        testService.deleteMails();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/tests/mails+jobs",
+            method = RequestMethod.POST)
+    public ResponseEntity<Object> dbMailsAndJobsPopulate(@RequestBody String set) {
+        if(testService.populateJobsAndMails(set)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/tests/mails/one",
+            method = RequestMethod.GET)
+    public ResponseEntity<Integer> dbMailArchivedId() {
+        Integer archivedMailId = testService.getOneMailId();
+        return new ResponseEntity<>(archivedMailId, HttpStatus.OK);
+    }
 }
--- swagger/spring-server-test/src/main/java/io/lozzikit/mail/service/TestService.java
+++ swagger/spring-server-test/src/main/java/io/lozzikit/mail/service/TestService.java
@@ -1,7 +1,144 @@
 package io.lozzikit.mail.service;
 
+import io.lozzikit.mail.entity.JobEntity;
+import io.lozzikit.mail.entity.MailEntity;
+import io.lozzikit.mail.entity.TemplateEntity;
+import io.lozzikit.mail.model.StatusEnum;
+import io.lozzikit.mail.repository.JobRepository;
+import io.lozzikit.mail.repository.MailRepository;
+import io.lozzikit.mail.repository.TemplateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class TestService {
+    @Autowired
+    private TemplateRepository templateRepository;
+    @Autowired
+    private MailRepository mailRepository;
+    @Autowired
+    private JobRepository jobRepository;
+
+    public void deleteTemplates() {
+        templateRepository.deleteAll();
+    }
+
+    public void deleteJobs() {
+        jobRepository.deleteAll();
+    }
+
+    public void deleteMails() {
+        mailRepository.deleteAll();
+    }
+
+    public boolean populateTemplates(String set) {
+        switch (set.toLowerCase()) {
+            case "default":
+                TemplateEntity template1 = new TemplateEntity();
+                template1.setName("test-template-1");
+                template1.setDescription("A first template for testing");
+                template1.setContent("Hello ${name}!");
+                templateRepository.save(template1);
+
+                TemplateEntity template2 = new TemplateEntity();
+                template2.setName("test-template-2");
+                template2.setDescription("A second template for testing");
+                template1.setContent("Goodbye ${name}!");
+                templateRepository.save(template2);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public boolean populateJobsAndMails(String set) {
+        switch (set.toLowerCase()) {
+            case "default":
+                final String from = "z.z@z.org";
+
+                Map<String, String> map;
+                List<String> dests;
+
+                MailEntity mail1 = new MailEntity();
+                mail1.setTemplateName("test-template-1");
+                mail1.setFrom(from);
+
+                dests = new ArrayList<>(Arrays.asList("y.y@y.org"));
+                mail1.setTo(dests);
+
+                map = new TreeMap<>();
+                map.put("name", "y");
+                mail1.setMap(map);
+                mailRepository.save(mail1);
+
+                MailEntity mail2 = new MailEntity();
+                mail2.setTemplateName("test-template-2");
+                mail2.setFrom(from);
+
+                dests = new ArrayList<>(Arrays.asList("x.x@x.org"));
+                mail2.setTo(dests);
+
+                map = new TreeMap<>();
+                map.put("name", "x");
+                mail2.setMap(map);
+                mailRepository.save(mail2);
+
+                MailEntity mail3 = new MailEntity();
+                mail3.setTemplateName("test-template-2");
+                mail3.setFrom(from);
+
+                dests = new ArrayList<>(Arrays.asList("v.v@v.org"));
+                mail3.setTo(dests);
+
+                map = new TreeMap<>();
+                map.put("name", "v");
+                mail3.setMap(map);
+                mailRepository.save(mail3);
+
+                MailEntity mail4 = new MailEntity();
+                mail4.setTemplateName("test-template-2");
+                mail4.setFrom(from);
+
+                dests = new ArrayList<>(Arrays.asList("w.w@w.org"));
+                mail4.setTo(dests);
+
+                map = new TreeMap<>();
+                map.put("name", "w");
+                mail4.setMap(map);
+                mailRepository.save(mail4);
+
+                JobEntity jobDone = new JobEntity();
+                jobDone.setStatus(StatusEnum.DONE);
+                jobDone.setMail(mail1);
+                jobRepository.save(jobDone);
+
+                JobEntity jobFailed = new JobEntity();
+                jobFailed.setStatus(StatusEnum.FAILED);
+                jobFailed.setMail(mail2);
+                jobRepository.save(jobFailed);
+
+                JobEntity jobInvalid = new JobEntity();
+                jobInvalid.setStatus(StatusEnum.INVALID);
+                jobInvalid.setMail(mail3);
+                jobRepository.save(jobInvalid);
+
+                JobEntity jobOngoing = new JobEntity();
+                jobOngoing.setStatus(StatusEnum.ONGOING);
+                jobOngoing.setMail(mail4);
+                jobRepository.save(jobOngoing);
+
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public Integer getOneMailId() {
+        List<MailEntity> mails = mailRepository.findAll();
+        return mails.get(0).getId();
+    }
 }
