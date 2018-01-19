8e9aa766479d54db56a486f5666522b9584458ab
Farenjihn <farenjihn@gmail.com>
2018-01-19 09:19:46 +0100

    Added unit tests

--- swagger/spring-server/src/test/java/io/lozzikit/mail/util/EntityFactoryTest.java
+++ swagger/spring-server/src/test/java/io/lozzikit/mail/util/EntityFactoryTest.java
@@ -2,41 +2,39 @@ package io.lozzikit.mail.util;
 
 import io.lozzikit.mail.api.model.MailDto;
 import io.lozzikit.mail.entity.MailEntity;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class EntityFactoryTest {
-
+public class EntityFactoryTest {
     @Test
-    void createFromMailDto() {
+    public void createFromMailDto() {
         MailDto dto = new MailDto();
         dto.from("test.one@testing.mail")
-                .addToItem("a.a@a.a")
-                .addToItem("b.b@b.b")
-                .addCcItem("c.c@c.c")
-                .addCcItem("d.d@d.d")
-                .addCciItem("e.e@e.e")
-                .addCciItem("f.f@f.f")
-                .templateName("test-template-1")
-                .putMapItem("name", "adam")
-                .putMapItem("event", "anniversary");
+            .addToItem("a.a@a.a")
+            .addToItem("b.b@b.b")
+            .addCcItem("c.c@c.c")
+            .addCcItem("d.d@d.d")
+            .addCciItem("e.e@e.e")
+            .addCciItem("f.f@f.f")
+            .templateName("test-template-1")
+            .putMapItem("name", "adam")
+            .putMapItem("event", "anniversary");
 
         MailEntity entity = EntityFactory.createFrom(dto);
 
-        assertEquals(dto.getFrom(), entity.getFrom());
-        assertIterableEquals(dto.getTo(), entity.getTo());
-        assertIterableEquals(dto.getCc(), entity.getCc());
-        assertIterableEquals(dto.getCci(), entity.getCci());
-        assertEquals(dto.getTemplateName(), entity.getTemplateName());
-        assertIterableEquals(dto.getMap().entrySet(), entity.getMap().entrySet());
+        Assert.assertEquals(dto.getFrom(), entity.getFrom());
+        Assert.assertArrayEquals(dto.getTo().toArray(), entity.getTo().toArray());
+        Assert.assertArrayEquals(dto.getCc().toArray(), entity.getCc().toArray());
+        Assert.assertArrayEquals(dto.getCci().toArray(), entity.getCci().toArray());
+        Assert.assertEquals(dto.getTemplateName(), entity.getTemplateName());
+        Assert.assertArrayEquals(dto.getMap().entrySet().toArray(), entity.getMap().entrySet().toArray());
     }
 
     @Test
-    void createFromJobDto() {
+    public void createFromJobDto() {
     }
 
     @Test
-    void createFromTemplateDto() {
+    public void createFromTemplateDto() {
     }
 }
