f4d8945d13a1009cdd3f07e9f4dcb01aeaa8458d
Lawrence <bykow12345@gmail.com>
2018-01-26 20:45:57 +0100

    Doc CliRunner

--- swagger/cucumber/src/test/java/io/lozzikit/mail/api/spec/CliRunner.java
+++ swagger/cucumber/src/test/java/io/lozzikit/mail/api/spec/CliRunner.java
@@ -4,6 +4,11 @@ import cucumber.api.cli.Main;
 
 public class CliRunner {
     public static void main(String[] args) throws Throwable {
+        /**
+         * Small hack to avoid compiling test in docker container,
+         * we compile them before creating the docker image and then
+         * link the test to the docker container
+         */
         String[] arguments = {"--glue", "io.lozzikit.mail.api.spec.steps", "classpath:scenarios"};
 
         Main.main(arguments);
