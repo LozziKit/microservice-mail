554e23ea128e0eaeb5f039e401b4167322b1fe22
Lawrence <bykow12345@gmail.com>
2018-01-28 12:54:25 +0100

    Removed interface, it's in another castle now...

--- interface/src/api/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    baseUrl: '',
-    contextPath: '/api/mail/v1/',
-};
-
-export default config;
--- interface/src/api/index.js
+++
@@ -1,5 +0,0 @@
-import TemplateApi from './templateApi.js';
-import JobApi from './jobApi.js';
-import MailApi from './mailApi.js';
-
-export { JobApi, TemplateApi, MailApi };
