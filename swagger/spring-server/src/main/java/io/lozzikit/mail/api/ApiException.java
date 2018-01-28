da3e6174a91cd350658d995d9aec6fffade5a713
Lawrence <bykow12345@gmail.com>
2018-01-28 17:46:59 +0100

    API Exception doc

--- swagger/spring-server/src/main/java/io/lozzikit/mail/api/ApiException.java
+++ swagger/spring-server/src/main/java/io/lozzikit/mail/api/ApiException.java
@@ -2,9 +2,18 @@ package io.lozzikit.mail.api;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-17T13:22:11.816Z")
 
+/**
+ * API Exception, used to define the errors
+ */
 public class ApiException extends Exception {
     private int code;
 
+    /**
+     * Ctor
+     *
+     * @param code code of the Exception
+     * @param msg message linked to code
+     */
     public ApiException(int code, String msg) {
         super(msg);
         this.code = code;
